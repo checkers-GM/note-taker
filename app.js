e4363db5cafdd4b6c98ea8a1646122b22b73f172
Nathan McCaw <nathanmccaw@Analias-Mac-mini.local>
2021-03-17 11:34:32 -0400

    setup basic server functions

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const PORT = 1234;
 const app = express();
+const dbJson = require("db.json");
+
+//inlude routes
+
+require("./apiRoutes");
+require("./htmlRoutes");
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
